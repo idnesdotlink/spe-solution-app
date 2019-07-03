c0c7a2db64333e4df76e7c9e9e0db597285eaad6
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-07-03 11:36:28 +0700

    clipboard

--- projects/ngx-clipboard-demo/src/lib/ngx-clipboard-demo.module.ts
+++ projects/ngx-clipboard-demo/src/lib/ngx-clipboard-demo.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NgxClipboardDemoComponent } from './ngx-clipboard-demo.component';
-
-
+import { ClipboardModule } from 'ngx-clipboard';
 
 @NgModule({
   declarations: [NgxClipboardDemoComponent],
   imports: [
+    ClipboardModule
   ],
   exports: [NgxClipboardDemoComponent]
 })
