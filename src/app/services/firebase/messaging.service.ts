399637524cdc8ebf31c912b04beb7e403a648026
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-07-19 10:56:08 +0700

    ok

---
+++ src/app/services/firebase/messaging.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+
+import * as take from 'rxjs/operator/take';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagingService {
+
+  messaging = firebase.messaging();
+  currentMessage = new BehaviorSubject(null)
+
+  constructor() { }
+}
