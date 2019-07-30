7e62fd7c232eb9e13311bcf4703b21400e9df92e
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-07-30 19:05:56 +0700

    dep

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "[apiKey]",
-    authDomain: "[projectId].firebaseapp.com",
-    databaseURL: "https://[projectId].firebaseio.com",
-    projectId: "[projectId]",
-    storageBucket: "[projectId].appspot.com",
-    messagingSenderId: "[messagingSenderId]",
-    appId: "[appId]"
+    apiKey: "AIzaSyDDZ8fgB5yyvi5Nbi4i9v1-yZKdRjqLqo4",
+    authDomain: "fbcx-bbc4d.firebaseapp.com",
+    databaseURL: "https://fbcx-bbc4d.firebaseio.com",
+    projectId: "fbcx-bbc4d",
+    storageBucket: "fbcx-bbc4d.appspot.com",
+    messagingSenderId: "819535897292",
+    appId: "1:819535897292:web:d920ee1f6e7141f5"
   }
 };
 
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/6.3.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/6.3.1/firebase-messaging.js");
 
 firebase.initializeApp({
-  messagingSenderId: "[messagingSenderId]"
+  messagingSenderId: "819535897292"
 });
 
 const messaging = firebase.messaging();
