25955c8329ee1cbd36c2734696c160b2ec1d9193
idnes.link <16096613+idnesdotlink@users.noreply.github.com>
2019-08-17 09:34:53 +0700

    ok

--- projects/ngx-charts/src/lib/common/label.helper.ts
+++ projects/ngx-charts/src/lib/common/label.helper.ts
@@ -1,6 +1,9 @@
 /**
  * Formats a label given a date, number or string.
- *
+ * 
+ * @export
+ * @param {*} label
+ * @returns {string}
  */
 export function formatLabel(label: any): string {
   if (label instanceof Date) {
@@ -10,4 +13,24 @@ export function formatLabel(label: any): string {
   }
 
   return label;
+}
+
+/**
+ * Escapes a label.
+ * 
+ * @export
+ * @param {*} label
+ * @returns {string}
+ */
+export function escapeLabel(label: any): string {
+  return label.toLocaleString().replace(/[&'`"<>]/g, match => {
+    return {
+      '&': '&amp;',
+      '\'': '&#x27;',
+      '`': '&#x60;',
+      '"': '&quot;',
+      '<': '&lt;',
+      '>': '&gt;',
+    }[match];
+  });
 }
